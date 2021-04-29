8b5f8f17a910a1818f9f155e5c3630cc62125bcc
Ivan <ivan5420@yandex.ru>
2021-04-29 13:19:46 +0300

    start

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge"/>
+  <link href="favicon.png" type="image/x-icon" rel="icon">
+  <link href="favicon.ico" type="image/x-icon" rel="shortcut icon">
+	<title>Атлас пациента клиники «МедЭксперт»</title>
+	<meta name="Description" content="Божественное описание страницы" />
+	<link rel="stylesheet" href="css/main.css">
+  <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+  <link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i&amp;subset=cyrillic" rel="stylesheet">
+
+
+
+    <!--<script src=""></script>-->
+
+</head>
+
+
+<body class="container">
+	<header>
+		<nav>
+			asdasd
+		</nav>
+		<nav>
+			asdasdasd
+		</nav>
+		<div class="header-white row">
+			<div class="col-50">
+				<img src="">
+			</div>
+			<div class="col-50 patient-fio">
+				<span>тестов иван иванович</span>
+			</div>
+		</div>
+</header>
+
+
+
+
+
+
+
+</body>
+</html>
